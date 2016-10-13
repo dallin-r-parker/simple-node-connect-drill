7b5d01300f86726fc6625a287444e9dc75255cc2
dallin-r-parker <dallin.r.parker@gmail.com>
2016-10-13 13:44:56 -0600

    morning lecture

--- server/controllers/chatCtrl.js
+++ server/controllers/chatCtrl.js
@@ -2,10 +2,16 @@ var chats = [];
 module.exports = {
 
   getChats:function(req,res){
-    // TODO write getChats
+      res.status(200).send(chats)
   },
   postChats:function(req,res){
     // TODO write postChats
+    var chatMessage = req.body;
+    var screenname = req.session.screenname;
+
+    chatMessage.screenname = sn;
+    chats.push(chatMessage);
+    res.status(200).end();
   },
   deleteChats:function(req,res){
     // TODO write deleteChats
--- server/server.js
+++ server/server.js
@@ -3,17 +3,19 @@ var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var chatCtrl = require('./controllers/chatCtrl');
+var keys = require('./secretkeys')
 var port = 3737;
 
 // TODO server front end files with static
-
+app.use(express.static('../public'));
 app.use(bodyParser.json());
-
+app.use(session({secret: keys.expressSecret}));
 // TODO Initialize Session
 
 app.post("/api/screenname", function(req, res){
-  // TODO Save screenname to session
-})
+      console.log('got screenname: ' + req.body.name);
+      req.session.screenname = req.body.name;
+});
 
 app.get("/api/chats", chatCtrl.getChats);
 app.post("/api/chats", chatCtrl.postChats);
